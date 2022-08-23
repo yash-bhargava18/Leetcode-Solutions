d84b43056b8bc92f0be096767e24061edb52f140
Yash Bhargava <98626961+yash-bhargava18@users.noreply.github.com>
2022-08-23 15:03:19 -0700

    Time: 216 ms (84.36%), Space: 18.5 MB (86.97%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,14 +1,13 @@
 class MyHashSet:
 
     def __init__(self):
-        self.size = 10000
+        self.size = 1000
         self.hashset = [None] * self.size
-
+        
     def hash_function(self,key):
         return key % self.size
-    
+
     def add(self, key: int) -> None:
-        
         hashed_value = self.hash_function(key)
         if self.hashset[hashed_value] == None:
             self.hashset[hashed_value] = [key]
@@ -16,19 +15,16 @@ class MyHashSet:
             self.hashset[hashed_value].append(key)
 
     def remove(self, key: int) -> None:
-        
         hashed_value = self.hash_function(key)
         if self.hashset[hashed_value] != None:
             while key in self.hashset[hashed_value]:
                 self.hashset[hashed_value].remove(key)
-                
 
     def contains(self, key: int) -> bool:
         hashed_value = self.hash_function(key)
         if self.hashset[hashed_value] != None:
             return key in self.hashset[hashed_value]
         return False
-        
 
 
 # Your MyHashSet object will be instantiated and called as such:
