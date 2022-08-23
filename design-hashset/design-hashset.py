7932e9794c53cf972a55ffb52e97ee2cde9ae18e
Yash Bhargava <98626961+yash-bhargava18@users.noreply.github.com>
2022-08-23 15:05:05 -0700

    Time: 199 ms, Memory: 18.5 MB - LeetHub

--- design-hashset/design-hashset.py
+++ design-hashset/design-hashset.py
@@ -3,27 +3,27 @@ class MyHashSet:
     def __init__(self):
         self.size = 1000
         self.hashset = [None] * self.size
-        
+    
     def hash_function(self,key):
         return key % self.size
 
     def add(self, key: int) -> None:
-        hashed_value = self.hash_function(key)
-        if self.hashset[hashed_value] == None:
-            self.hashset[hashed_value] = [key]
+        hashed_function = self.hash_function(key)
+        if self.hashset[hashed_function] == None:
+            self.hashset[hashed_function] = [key]
         else:
-            self.hashset[hashed_value].append(key)
+            self.hashset[hashed_function].append(key)
 
     def remove(self, key: int) -> None:
-        hashed_value = self.hash_function(key)
-        if self.hashset[hashed_value] != None:
-            while key in self.hashset[hashed_value]:
-                self.hashset[hashed_value].remove(key)
+        hashed_function = self.hash_function(key)
+        if self.hashset[hashed_function] != None:
+            while key in self.hashset[hashed_function]:
+                self.hashset[hashed_function].remove(key) 
 
     def contains(self, key: int) -> bool:
-        hashed_value = self.hash_function(key)
-        if self.hashset[hashed_value] != None:
-            return key in self.hashset[hashed_value]
+        hashed_function = self.hash_function(key)
+        if self.hashset[hashed_function] != None:
+            return key in self.hashset[hashed_function]
         return False
 
 
